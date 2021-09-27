5bcdfb0236adc0c681400751d07263d96fb2410f
IshradShaikh <84022826+IshradShaikh@users.noreply.github.com>
2021-09-27 15:27:20 +0530

    ..

--- README.md
+++
@@ -1,2 +0,0 @@
-# React_Project
-git repo
--- src/store/reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import auth from '../account/reducerAuth';
-
-const rootReducer = combineReducers({
-  auth
-});
-
-export default rootReducer;
